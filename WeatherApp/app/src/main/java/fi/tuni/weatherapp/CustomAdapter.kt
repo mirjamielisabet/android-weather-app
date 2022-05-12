b29f8ac419c61cac3fb32941aa08b7ff257b8e80
Mirjami Laiho <mirjami.laiho@tuni.fi>
2022-05-12 19:11:06 +0300

    Modify the format of the date and time by using DateTimeFormatter and move the concatenated strings (temp and wind speed) to the strings.xml file

--- WeatherApp/app/src/main/java/fi/tuni/weatherapp/CustomAdapter.kt
+++ WeatherApp/app/src/main/java/fi/tuni/weatherapp/CustomAdapter.kt
@@ -1,13 +1,19 @@
 package fi.tuni.weatherapp
 
 import android.content.Context
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.collections.ArrayList
+
 
 class CustomAdapter(private var context: Context, private var data:  ArrayList<ForecastList>)
     : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
@@ -25,12 +31,18 @@ class CustomAdapter(private var context: Context, private var data:  ArrayList<F
             .inflate(R.layout.list_item, viewGroup, false)
         return ViewHolder(view)
     }
-
+    
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.date.text = data[position].dt_txt
-        viewHolder.temp.text = data[position].main.temp.toString()
+        val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
+        val currentFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        val date = LocalDateTime.parse(data[position].dt_txt, currentFormat)
+        val formattedDate = formatter.format(date)
+
+        viewHolder.date.text = formattedDate
+        viewHolder.temp.text = context.getString(R.string.forecastTemp, data[position].main.temp)
         viewHolder.desc.text = data[position].weather[0].description
-        viewHolder.wind.text = data[position].wind.speed.toString()
+        viewHolder.wind.text = context.getString(R.string.forecastWind, data[position].wind.speed)
         Glide.with(context)
             .load("https://openweathermap.org/img/w/${data[position].weather[0].icon}.png")
             .into(viewHolder.icon)
