3974a326e154b95b69af26c7690db68801d2f99a
Mirjami Laiho <mirjami.laiho@tuni.fi>
2022-05-19 19:26:45 +0300

    Add documentation

--- WeatherApp/app/src/main/java/fi/tuni/weatherapp/Weather.kt
+++ WeatherApp/app/src/main/java/fi/tuni/weatherapp/Weather.kt
@@ -2,14 +2,40 @@ package fi.tuni.weatherapp
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
+/**
+ * Represents the weather.
+ *
+ * @property description the description of the weather
+ * @property icon the identifier of the weather icon
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Weather(var description: String, var icon: String)
 
+/**
+ * Represents the result of the weather data API call.
+ *
+ * @property weather list of information about the weather
+ * @property main Temperature object
+ * @property wind Wind object
+ * @property name the name of the location
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class WeatherResult(var weather: MutableList<Weather>, var main: Temperature, var wind: Wind, var name: String)
 
+/**
+ * Represents the temperature and it's related factors.
+ *
+ * @property temp the outside temperature
+ * @property feels_like the feels-like temperature
+ * @property humidity the number describing the humidity of the air
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Temperature(var temp: Double, var feels_like: Double, var humidity: Int)
 
+/**
+ * Represents the wind.
+ *
+ * @property speed the speed of the wind
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Wind(var speed: Double)
