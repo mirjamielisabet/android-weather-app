d8daba2c7b4c7819bdd5c78799c110a765da5fa0
Mirjami Laiho <mirjami.laiho@tuni.fi>
2022-05-19 18:44:50 +0300

    Add documentation

--- WeatherApp/app/src/main/java/fi/tuni/weatherapp/CustomAdapter.kt
+++ WeatherApp/app/src/main/java/fi/tuni/weatherapp/CustomAdapter.kt
@@ -14,10 +14,20 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import kotlin.collections.ArrayList
 
-
+/**
+ * Adapter for the recycler view.
+ *
+ * @param context the activity context
+ * @param data the list of the forecast data
+ */
 class CustomAdapter(private var context: Context, private var data:  ArrayList<ForecastList>)
     : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
+    /**
+     * The ViewHolder that holds all the view references.
+     *
+     * @param view the view inflated.
+     */
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val date: TextView = view.findViewById(R.id.date)
         val temp: TextView = view.findViewById(R.id.forecastTemp)
@@ -26,12 +36,28 @@ class CustomAdapter(private var context: Context, private var data:  ArrayList<F
         val wind: TextView = view.findViewById(R.id.forecastWind)
     }
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+    /**
+     * Sets the views to display the items.
+     * Inflates the view from it's layout and passes it to the ViewHolder.
+     *
+     * @param viewGroup the parent view
+     * @param viewType the type of the view
+     * @return ViewHolder with inflated view as a parameter.
+     */
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int) : ViewHolder {
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.list_item, viewGroup, false)
         return ViewHolder(view)
     }
 
+    /**
+     * Binds the list items to the views (widgets).
+     * Formats the date by using DateTimeFormatter before binding it to it's TextView.
+     * Glide is used for loading the weather icon from OpenWeather website.
+     *
+     * @param viewHolder the ViewHolder that holds all the views needed for presenting the data.
+     * @param position the position of the item.
+     */
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
@@ -48,5 +74,10 @@ class CustomAdapter(private var context: Context, private var data:  ArrayList<F
             .into(viewHolder.icon)
     }
 
+    /**
+     * Returns the count of items in the list.
+     *
+     * @return the size of the data.
+     */
     override fun getItemCount() = data.size
 }
