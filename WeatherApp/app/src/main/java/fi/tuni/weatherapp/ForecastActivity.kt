cb4c7462fc1d2dece2f8a4e53896dc006c4bd336
Mirjami Laiho <mirjami.laiho@tuni.fi>
2022-05-19 16:09:59 +0300

    Add documentation and move the heading text to the strings.xml file

--- WeatherApp/app/src/main/java/fi/tuni/weatherapp/ForecastActivity.kt
+++ WeatherApp/app/src/main/java/fi/tuni/weatherapp/ForecastActivity.kt
@@ -11,16 +11,22 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.lang.Exception
 
+/**
+ * Activity that presents the forecast results to the user.
+ */
 class ForecastActivity : AppCompatActivity() {
     lateinit var heading : TextView
     lateinit var key : String
     lateinit var location : String
     lateinit var recyclerView: RecyclerView
     lateinit var adapter : CustomAdapter
-    var data = ArrayList<ForecastList>()
+    private var data = ArrayList<ForecastList>()
+    private var errmsg = ""
 
-    var errmsg = ""
-
+    /**
+     * When the activity is starting, initializes the variables, sets the heading text and gets
+     * the location from the Intent. Adds a divider between the recycler view's list items.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast)
@@ -30,7 +36,7 @@ class ForecastActivity : AppCompatActivity() {
         heading = findViewById(R.id.forecastHeading)
         key = getString(R.string.key)
         location = intent.getStringExtra("location").toString()
-        heading.text = "$location - 5 Day Forecast"
+        heading.text = getString(R.string.forecastHeading, location)
 
         adapter = CustomAdapter(this, data)
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -38,11 +44,21 @@ class ForecastActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
     }
 
+    /**
+     * When the activity becomes active, calls the getForecastData function that fetches the
+     * forecast results.
+     */
     override fun onResume() {
         super.onResume()
         getForecastData()
     }
 
+    /**
+     * Uses the downloadUrlAsync function from the UrlConnection class for fetching the forecast
+     * results from the OpenWeather API. If the results are fetched successfully, the forecast
+     * list item is added to the recycler view by using the CustomAdapter. If the results are not
+     * fetched successfully, the error message is shown to the user.
+     */
     fun getForecastData() {
         val url = "https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=${key}"
         UrlConnection().downloadUrlAsync(this, url) { json ->
@@ -65,6 +81,13 @@ class ForecastActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * By using Jackson's ObjectMapper, parses the forecast json into the ForecastResult
+     * object. Returns the formed object or null if exception is caught.
+     *
+     * @param json the forecast result json to be parsed.
+     * @return the ForecastResult object that contains the forecast or null if error occurs.
+     */
     fun parseForecastJSON(json: String): ForecastResult? {
         return try {
             val mapper = ObjectMapper().registerKotlinModule()
